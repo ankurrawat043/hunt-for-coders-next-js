2b257381a7ecade64099a13d66232948d2da4c83
ANKUR RAWAT <ankur.rawatking43@gmail.com>
2023-06-20 10:50:23 +0530

    project completed

---
+++ components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+
+function Navbar() {
+  return (
+    <nav className={styles.mainnav}>
+      <ul>
+        <Link href="/">
+          <li>Home</li>
+        </Link>
+        <Link href="/about">
+          <li>About</li>
+        </Link>
+        <Link href="/blog">
+          <li>Blogs</li>
+        </Link>
+        <Link href="/contact">
+          <li>Contact</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
